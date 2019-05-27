12e2c3909a40bc2bf2058fc5e09fee5fac47c965
datavistics <datavistics@gmail.com>
2019-05-27 19:49:04 +0400

    Adding get_replays for convenience

---
+++ hand_crafted/get_game_ids.py
@@ -0,0 +1,38 @@
+import pickle
+from datetime import datetime
+from pprint import pprint
+from time import sleep
+
+import requests
+
+
+def get_game_ids(username, count):
+    """
+    You can get count as a multiple of 20. Fn will round up: 39-> 40
+    :param username:
+    :param count:
+    :return:
+    """
+    page_size = 20
+    ids = []
+    for offset in range(0, count, page_size):
+        url = f"http://generals.io/api/replaysForUsername?u={username}&offset={offset}&count={page_size}"
+        r = requests.get(url)
+        response_json = r.json()
+        response_json_subset = [
+            {'started': datetime.utcfromtimestamp(js['started'] / 1000).strftime('%Y-%m-%d %H:%M:%S'),
+             'id': js['id']}
+            for js in response_json]
+        ids.extend(response_json_subset)
+        sleep(2)
+        print(f'Retrieved {offset + 20} total game ids')
+
+    pprint(ids)
+    return ids
+
+
+if __name__ == '__main__':
+    username = 'Spraget'
+    ids = get_game_ids(username, 2000)
+    with open(username + '.pkl', 'wb') as f:
+        pickle.dump(ids, f)
